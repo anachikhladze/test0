67cd2ad004f3ddfa886db9293388d8268d9dc34b
Anna Sumire <ana@Anas-MacBook-Air.local>
2023-11-28 17:47:23 +0400

    testing project

---
+++ TestProject0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject0
+//
+//  Created by Anna Sumire on 28.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
